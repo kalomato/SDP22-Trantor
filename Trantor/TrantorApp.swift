707b38ffb5f92ea253448b3a9e4ce210d6c9b081
Enrique <enrique@tizona.net>
2023-02-05 19:11:59 +0100

    Initial Commit

---
+++ Trantor/TrantorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrantorApp.swift
+//  Trantor
+//
+//  Created by Enrique on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TrantorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
