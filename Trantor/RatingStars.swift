429dd286e20b69b353441fa86b3af2f3d82cb10e
Enrique <enrique@tizona.net>
2023-02-25 22:10:15 +0100

    20230225-2210

--- Trantor/RatingStars.swift
+++ Trantor/RatingStars.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RatingStars: View {
     let rating: Double
+    let size: CGFloat = 14
 
     var body: some View {
         HStack(spacing: 0) {
@@ -16,12 +17,15 @@ struct RatingStars: View {
                 if rating >= Double(index) {
                     Image(systemName: "star.fill")
                         .foregroundColor(.yellow)
+                        .font(.system(size: size))
                 } else if rating >= Double(index) - 0.5 {
                     Image(systemName: "star.leadinghalf.fill")
                         .foregroundColor(.yellow)
+                        .font(.system(size: size))
                 } else {
                     Image(systemName: "star")
                         .foregroundColor(.yellow)
+                        .font(.system(size: size))
                 }
             }
         }
